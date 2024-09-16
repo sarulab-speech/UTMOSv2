8ff4e508d81dcfe1bfb8160ed4279ca66470e64d
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-09-17 06:33:13 +0900

    Add public api

---
+++ utmosv2/_core/__init__.py
@@ -0,0 +1,5 @@
+from utmosv2._core.create import create_model
+from utmosv2._core.model import UTMOSv2Model
+from utmosv2._core.model._common import UTMOSv2ModelMixin
+
+__all__ = ["UTMOSv2Model", "UTMOSv2ModelMixin", "create_model"]
---
+++ utmosv2/_core/model/__init__.py
@@ -0,0 +1,4 @@
+from utmosv2._core.model._common import UTMOSv2ModelMixin
+from utmosv2._core.model._models import UTMOSv2Model
+
+__all__ = ["UTMOSv2Model", "UTMOSv2ModelMixin"]
