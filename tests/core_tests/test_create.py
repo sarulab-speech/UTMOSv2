aa9cf6c462ca48ad8641d0fa266064e47dea8f74
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-02-25 23:57:50 +0000

    Apply automatic code formatting

--- tests/core_tests/test_create.py
+++ tests/core_tests/test_create.py
@@ -5,14 +5,21 @@ from utmosv2._core.create import create_model
 
 import sys
 
+
 @pytest.mark.parametrize(
-    "pretrained", [
-        pytest.param(True, marks=pytest.mark.skipif(sys.version_info[:2] != (3, 11), reason="To avoid downloading the model weights multiple times")),
-        False]
+    "pretrained",
+    [
+        pytest.param(
+            True,
+            marks=pytest.mark.skipif(
+                sys.version_info[:2] != (3, 11),
+                reason="To avoid downloading the model weights multiple times",
+            ),
+        ),
+        False,
+    ],
 )
 def test_create_model(pretrained: bool) -> None:
-    model = create_model(
-        pretrained=pretrained
-    )
+    model = create_model(pretrained=pretrained)
     assert hasattr(model, "forward")
     assert hasattr(model, "predict")
