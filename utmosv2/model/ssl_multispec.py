ca417a5af41328a52d450081b1d4ca09cef2d40a
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-02-25 20:31:24 +0900

    Update type annotations for mypy

--- utmosv2/model/ssl_multispec.py
+++ utmosv2/model/ssl_multispec.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from typing import cast
 
 from utmosv2._settings._config import Config
 from utmosv2.dataset._utils import get_dataset_num
@@ -27,15 +28,13 @@ class SSLMultiSpecExtModelV1(nn.Module):
                 param.requires_grad = False
             for param in self.spec_long.parameters():
                 param.requires_grad = False
-        ssl_input = self.ssl.fc.in_features
-        spec_long_input = self.spec_long.fc.in_features
         self.ssl.fc = nn.Identity()
         self.spec_long.fc = nn.Identity()
 
         self.num_dataset = get_dataset_num(cfg)
 
         self.fc = nn.Linear(
-            ssl_input + spec_long_input + self.num_dataset,
+            cast(int, self.ssl.fc.in_features) + cast(int, self.spec_long.fc.in_features) + self.num_dataset,
             cfg.model.ssl_spec.num_classes,
         )
 
@@ -70,15 +69,13 @@ class SSLMultiSpecExtModelV2(nn.Module):
                 param.requires_grad = False
             for param in self.spec_long.parameters():
                 param.requires_grad = False
-        ssl_input = self.ssl.fc.in_features
-        spec_long_input = self.spec_long.fc.in_features
         self.ssl.fc = nn.Identity()
         self.spec_long.fc = nn.Identity()
 
         self.num_dataset = get_dataset_num(cfg)
 
         self.fc = nn.Linear(
-            ssl_input + spec_long_input + self.num_dataset,
+            cast(int, self.ssl.fc.in_features) + cast(int, self.spec_long.fc.in_features) + self.num_dataset,
             cfg.model.ssl_spec.num_classes,
         )
 
